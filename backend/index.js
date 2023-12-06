ef9635042385af6304fdda53ac390cd08eb487b3
Brayan Marin Guirales <brayan6590@gmail.com>
2023-12-06 11:01:53 -0500

    Finalizando desarrollo Backend...

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
 import conectarDB from "./config/db.js";
+import pacienteRoutes from "./routes/pacienteRoutes.js";
 import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 const app = express();
 app.use(express.json());
 dotenv.config();
 conectarDB();
 app.use("/api/veterinarios", veterinarioRoutes);
+app.use("/api/pacientes", pacienteRoutes);
 
 const PORT = process.env.PORT || 4000;
 
